cef56ee9ba612f2cc0fa39c0c8dc560572e39b23
Canh Tran <canh.tran@s3corp.com.vn>
2020-08-21 16:35:57 +0700

    hello

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
+import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import HomeScreen from "./src/screens/HomeScreen";
+import IndexScreen from "./src/screens/IndexScreen";
+import {BlogProvider} from './src/context/BlogContext'
 
 const navigator = createStackNavigator(
   {
-    Home: HomeScreen
+    Index: IndexScreen
   },
   {
-    initialRouteName: "Home",
+    initialRouteName: "Index",
     defaultNavigationOptions: {
-      title: "App"
+      title: "Blogs"
     }
   }
 );
 
-export default createAppContainer(navigator);
+const App = createAppContainer(navigator);
+
+export default () => {
+  return (
+    <BlogProvider>
+      <App />
+    </BlogProvider>
+  )
+}
